ededcc8f0921cdce8b2b4dbba648c9acf437250c
Peter Hollaubek <peter.hollaubek@gmail.com>
2025-02-04 18:35:31 -0500

    get database

--- lib/notion.py
+++ lib/notion.py
@@ -4,19 +4,20 @@ import configparser
 from pprint import pprint
 from dataclasses import dataclass, field
 from lib.database import Database
+from lib.common import get_config_value, get_logger
+
+logger = get_logger()
 
 @dataclass
 class Notion:
     token: str = field(init=False, repr=False)
     def __post_init__(self):
-        """Get token from ~/.notion.ini"""
-        home = os.path.expanduser("~")
-        config = configparser.ConfigParser()
-        config.read(f"{home}/.notion.ini")
-        if "DEFAULT" in config and "token" in config["DEFAULT"]:
-            self.token = config["DEFAULT"]["token"]
-        else:
-            raise ValueError("Token not found in ~/.notion.ini")
+        """Get token from credentials ini file"""
+        try:
+            self.token = get_config_value(section="notion", key="token")
+        except configparser.Error as exception:
+            logger.error(f"Could not get Notion token: {exception}")
+            exit(1)
         
     def call_api(self, method, path, data=None):
         """Call Notion API"""
@@ -37,9 +38,14 @@ class Notion:
         else:
             raise ValueError("Invalid method")
         
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as exception:
+            logger.error(exception)
+
         return response
     
-    def get_databases(self):
+    def get_databases(self, query: str = None):
         """Get all databases"""
         data = {
             "filter": {
@@ -47,8 +53,18 @@ class Notion:
                 "value": "database"
             }
         }
+        if query:
+            data["query"] = query
+
         response = self.call_api("POST", "/search", data=data)
         
         for database_data in response.json().get("results", []):
             database = Database(notion=self, id=database_data["id"], data=database_data)
             yield database
+
+    def get_database(self, title: str):
+        """Get database by title."""
+        for database in self.get_databases(query=title):
+            print(database.title, title)
+            if database.title == title:
+                return database
